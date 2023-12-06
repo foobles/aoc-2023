49f49a1c43ffa9b3f262309f02c11e6784e99af4
April <buzdav2@gmail.com>
2023-12-06 10:29:47 -0800

    Add gitattributes and move out input files

--- src/day1.s
+++ src/day1.s
@@ -4,7 +4,7 @@
 .segment "CODE0"
 .include "day1_impl.s"
 input_data:
-.incbin "day1_input_a.txt"
+.incbin "input/day1_a.txt"
 .byte $00, $01 ; sentinel + next bank to switch to
 
 .segment "CODE1"
@@ -12,5 +12,5 @@ input_data:
 .include "day1_impl.s"
 .endscope
 .assert * = input_data, error
-.incbin "day1_input_b.txt"
+.incbin "input/day1_b.txt"
 .byte $00, $FF ; sentinel + indicate done
